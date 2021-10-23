b5f1351bb35dddfbe765376020e13ff67c989ba3
jamesee <james.ee.developer@gmail.com>
2021-10-23 20:51:14 +0800

    deploy ansible

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# if [ ! -z "$ANSIBLE_VAULT_PASSWORD" ]
-# then
-#       echo $ANSIBLE_VAULT_PASSWORD >> .vault;
-#       ansible-playbook $1 -i $2 --vault-password-file .vault;
-#       rm .vault
-# else
-#       ansible-playbook $1 -i $2
-# fi
+echo $SSH_PRIVATE_KEY > ~/.ssh/james-sutd-ec2.pem
+chmod 400 ~/.ssh/james-sutd-ec2.pem
 
-echo $SSH_PRIVATE_KEY
+if [ ! -z "$ANSIBLE_VAULT_PASSWORD" ]
+then
+      echo $ANSIBLE_VAULT_PASSWORD >> .vault;
+      ansible-playbook $1 -i $2 --vault-password-file .vault;
+      rm .vault
+else
+      ansible-playbook $1 -i $2
+fi
+

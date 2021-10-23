73a64be0f1623c899f7a6c069b56475f71d399bb
jamesee <james.ee.developer@gmail.com>
2021-10-23 21:22:20 +0800

    deploy ansible

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -1,8 +1,26 @@
-#!/bin/bash
+#!/bin/sh
+set -eu
+
+SSHPATH="$HOME/.ssh"
+
+if [ ! -d "$SSHPATH" ]
+then
+  mkdir "$SSHPATH"
+fi
+
+if [ ! -f "$SSHPATH/known_hosts" ]
+then
+  touch "$SSHPATH/known_hosts"
+fi
+
+echo "$INPUT_KEY" > "$SSHPATH/james-sutd-ec2.pem"
+KEYFILE="$SSHPATH/james-sutd-ec2.pem"
 
 
-echo $SSH_PRIVATE_KEY > james-sutd-ec2.pem
-chmod 400 james-sutd-ec2.pem
+chmod 700 "$SSHPATH"
+chmod 600 "$SSHPATH/known_hosts"
+chmod 600 "$SSHPATH/james-sutd-ec2.pem"
+
 
 if [ ! -z "$ANSIBLE_VAULT_PASSWORD" ]
 then

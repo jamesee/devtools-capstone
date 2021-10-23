c93dddf342016516f057aee80d12864e47e4d4f0
jamesee <james.ee.developer@gmail.com>
2021-10-23 21:47:13 +0800

    deploy ansible

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -13,15 +13,15 @@ then
   touch "$SSHPATH/known_hosts"
 fi
 
-echo "$INPUT_KEY" > "$SSHPATH/deploy_key"
-KEYFILE="$SSHPATH/deploy_key"
+echo "$INPUT_KEY" > "$SSHPATH/james-sutd-ec2.pem"
+KEYFILE="$SSHPATH/james-sutd-ec2.pem"
 
 ls -al ~/.ssh
 pwd
 
 chmod 700 "$SSHPATH"
 chmod 600 "$SSHPATH/known_hosts"
-chmod 600 "$SSHPATH/deploy_key"
+chmod 600 "$SSHPATH/james-sutd-ec2.pem"
 
 sh -c "ssh -i $KEYFILE -o StrictHostKeyChecking=no -p 22 ubuntu@18.136.101.16 "
 

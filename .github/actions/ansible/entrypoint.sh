4b5f6f1028c7f961397c04de9cf23008e1e2a1eb
jamesee <james.ee.developer@gmail.com>
2021-10-24 17:06:55 +0800

    workflows: deploy-ansible.yml

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -20,28 +20,8 @@ chmod 700 "$SSHPATH"
 chmod 600 "$SSHPATH/known_hosts"
 chmod 400 "$SSHPATH/james-sutd-ec2.pem"
 
-# chown 1001:121 "$SSHPATH/known_hosts"
-# chown 1001:121 "$SSHPATH/james-sutd-ec2.pem"
-
-
-# echo $HOME
-# echo $KEYFILE
-# ls -al $HOME/.ssh
-
-
 # sh -c "ssh -i $KEYFILE -o StrictHostKeyChecking=no -p 22 ubuntu@18.136.101.16 "
-
-# cat $INPUT_INVENTORY
-# cat $INPUT_PLAYBOOK
 
 ansible-playbook "$INPUT_PLAYBOOK" -i "$INPUT_INVENTORY"
 
-# if [ ! -z "$ANSIBLE_VAULT_PASSWORD" ]
-# then
-#       echo $ANSIBLE_VAULT_PASSWORD >> .vault;
-#       ansible-playbook $1 -i $2 --vault-password-file .vault;
-#       rm .vault
-# else
-#       ansible-playbook $1 -i $2
-# fi
 

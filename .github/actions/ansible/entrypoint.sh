62a10d3eb4424696624bc7faa97e0e9b51dadab5
jamesee <james.ee.developer@gmail.com>
2021-10-24 16:05:44 +0800

    deploy ansible : entrypoint.sh

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -29,12 +29,12 @@ echo $KEYFILE
 ls -al $HOME/.ssh
 
 
-sh -c "ssh -i $KEYFILE -o StrictHostKeyChecking=no -p 22 ubuntu@18.136.101.16 "
+# sh -c "ssh -i $KEYFILE -o StrictHostKeyChecking=no -p 22 ubuntu@18.136.101.16 "
 
 cat $INPUT_INVENTORY
 cat $INPUT_PLAYBOOK
 
-# ansible-playbook "$INPUT_PLAYBOOK" -i "$INPUT_INVENTORY"
+ansible-playbook "$INPUT_PLAYBOOK" -i "$INPUT_INVENTORY"
 
 # if [ ! -z "$ANSIBLE_VAULT_PASSWORD" ]
 # then

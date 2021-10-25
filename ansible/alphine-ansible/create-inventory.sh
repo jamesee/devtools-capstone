c208adc559bc3532219280beea180a798da947aa
jamesee <james.ee.developer@gmail.com>
2021-10-25 19:32:43 +0800

    debug:james1122/ansible-in-containers

--- ansible/alphine-ansible/create-inventory.sh
+++ ansible/alphine-ansible/create-inventory.sh
@@ -4,12 +4,12 @@
 # then
 #     OUTPUT=`terraform show -json | jq ".values .root_module .resources | last .values .content"` 
 # else
-    OUTPUT=`cat ansible/terraform-outputs.json | jq ".values .root_module .resources | last .values .content"`
+    OUTPUT=`cat terraform-outputs.json | jq ".values .root_module .resources | last .values .content"`
 # fi
 
 FINAL=`sed -e 's/^"//' -e 's/"$//' <<< "$OUTPUT"`
 
-# echo -e "$FINAL"
-echo -e "$FINAL" | tee ./ansible/inventory.ini 
+echo -e "$FINAL"
+# echo -e "$FINAL" | tee ./ansible/inventory.ini 
 
 # cat ../ansible/inventory.ini 

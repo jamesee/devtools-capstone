196b94aae68c1a72098bb2bf130c50abe01bcacb
jamesee <james.ee.developer@gmail.com>
2021-10-25 18:19:01 +0800

    workflows: terraform.yml

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -20,21 +20,13 @@ chmod 700 "$SSHPATH"
 chmod 600 "$SSHPATH/known_hosts"
 chmod 400 "$SSHPATH/james-sutd-ec2.pem"
 
-ls -al 
-ls -al ansible/
+# ls -al 
+# ls -al ansible/
 # sh -c "ssh -i $KEYFILE -o StrictHostKeyChecking=no -p 22 ubuntu@18.136.101.16 "
 
 cp ./ansible/ansible.cfg .
 
-# ls -al 
-# ls -al terraform/
-
-# cd terraform
 bash ./terraform/create-inventory.sh
-# cd ..
-
-# ls -al 
-# ls -al ansible/
 
 ansible-playbook "$INPUT_PLAYBOOK" -i "$INPUT_INVENTORY"
 

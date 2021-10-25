eb4d1a87729e1d262e73096ecc55645ac3f0fcd8
jamesee <james.ee.developer@gmail.com>
2021-10-25 22:43:03 +0800

    workflows:terraform-ansible-IaC.yml

--- ansible/create-inventory.py
+++ ansible/create-inventory.py
@@ -1,11 +1,15 @@
 #!/usr/local/bin/python3
+
 import json
 
 with open('./ansible/terraform-outputs.json') as f:
   data = json.load(f)
 
-print(data["values"]["outputs"]["ansible_inventory_file"]["value"]["content"])
+# print(data["values"]["outputs"]["ansible_inventory_file"]["value"]["content"])
+print(data["content"])
 
-inventory = data["values"]["outputs"]["ansible_inventory_file"]["value"]["content"]
+
+# inventory = data["values"]["outputs"]["ansible_inventory_file"]["value"]["content"]
+
 with open('./ansible/inventory.ini', 'w') as output_file:
-  output_file.write(inventory)
+  output_file.write(data["content"])

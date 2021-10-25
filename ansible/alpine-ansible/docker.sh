977c94c672d1ac418a231c3805c6f44043c2ffda
jamesee <james.ee.developer@gmail.com>
2021-10-25 22:02:17 +0800

    ansible:create-inventory.py

--- ansible/alpine-ansible/docker.sh
+++ ansible/alpine-ansible/docker.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker run --entrypoint entrypoint.sh -it --rm --volume ${PWD}:/ansible -w /ansible  ansible-in-containers
+docker run --entrypoint bash -it --rm --volume ${PWD}:/ansible -w /ansible  james1122/ansible-in-containers
+
+docker run -it --rm --volume ~/.ssh:/root/.ssh --volume ${PWD}:/ansible -w /ansible james1122/ansible-in-containers 

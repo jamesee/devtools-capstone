21b01ce992075d3ee9877c2379eaea07d313bab8
jamesee <james.ee.developer@gmail.com>
2021-10-26 08:50:31 +0800

    ansible: create-inventory.py

--- ansible/create-inventory.py
+++ ansible/create-inventory.py
@@ -7,8 +7,10 @@ with open('./ansible/test.json') as f:
 
 print(data_test)
 
-with open('./ansible/terraform-outputs.json') as f:
-  data = json.load(f)
+with open('./ansible/terraform-outputs.txt') as f:
+  # data = json.load(f)
+  data = f.read()
+  
 
 # print(data["values"]["outputs"]["ansible_inventory_file"]["value"]["content"])
 print(data)

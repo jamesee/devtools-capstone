56945d35845b1a4aa4edf3bbb93195f60938091d
jamesee <james.ee.developer@gmail.com>
2021-10-27 09:53:03 +0800

    test: uploaded terraform-outputs.json - create-inventory.py

--- ansible/create-inventory.py
+++ ansible/create-inventory.py
@@ -9,18 +9,19 @@ print(data_test)
 
 with open('./ansible/terraform-outputs.json') as f:
   # print(f.read())
-  data = json.load(f)
+  # data = json.load(f)
   
-  # data = f.read()
-  # dataJson = json.loads(data)
+  data = f.read()
+  print(data)
+  dataJson = json.loads(data)
 
 # print(data["values"]["outputs"]["ansible_inventory_file"]["value"]["content"])
-print(data)
-# print(dataJson)
+# print(data)
+print(dataJson)
 
 
 # inventory = data["values"]["outputs"]["ansible_inventory_file"]["value"]["content"]
 
 with open('./ansible/inventory.ini', 'w') as output_file:
-  output_file.write(data["content"])
-  # output_file.write(dataJson["content"])
+  # output_file.write(data["content"])
+  output_file.write(dataJson["content"])

bfec5823d1dab80575222345e9f1709ee395eeb0
jamesee <james.ee.developer@gmail.com>
2021-11-08 21:19:19 +0800

    made changes to server.js

--- server.js
+++ server.js
@@ -4,6 +4,23 @@ const router = jsonServer.router(process.env.DB || 'db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+
+// Add custom routes before JSON Server router
+server.get('/echo', (req, res) => {
+    res.jsonp(req.query)
+  })
+  
+  // To handle POST, PUT and PATCH you need to use a body-parser
+  // You can use the one used by JSON Server
+  server.use(jsonServer.bodyParser)
+  server.use((req, res, next) => {
+    if (req.method === 'POST') {
+      req.body.createdAt = Date.now()
+    }
+    // Continue to JSON Server router
+    next()
+  })
+  
 server.use(router);
 
 module.exports = server;

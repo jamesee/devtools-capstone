33680b1944778db07e4308819c927146bb17d25c
jamesee <james.ee.developer@gmail.com>
2021-11-08 21:07:00 +0800

    replace index.html

--- ansible/app/check-docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# if [ "$(docker ps -aq -f status=exited -f name=$1*)" ]; then
-#     docker rm -f $(docker ps -q -f name="$1*") \
-# fi
-
-if [ "$(docker ps -q -f name={{ default_container_name }}*" ]; then 
-    echo "$(docker ps -q -f name={{ default_container_name }}*"
-    docker rm -f $(docker ps -q -f name="{{ default_container_name }}*") 
-fi
